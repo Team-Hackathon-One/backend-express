731fb0e196d161632a1c26653d20834d1988acbd
faq <facundomajda13@gmail.com>
2024-08-23 07:16:14 -0300

    chore: added methods in plant controller

--- src/controllers/plant.controller.js
+++ src/controllers/plant.controller.js
@@ -4,7 +4,39 @@ import { env } from "../config/env";
 const API_KEY = env.plant_api_key;
 const API_URL = "https://plant.id/api/v3";
 
-export async function identifyPlant(req, res) {
+export function searchPlant(req, res) {
+  const { query, limit } = req.query;
+
+  try {
+    fetch(
+      `${API_URL}/kb/plants/name_search?q=${query}&limit=${
+        limit || 10
+      }&language=es`,
+      {
+        method: "GET",
+        headers: {
+          "Api-Key": API_KEY,
+        },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((jsonData) => res.json(jsonData))
+      .catch((error) => {
+        console.error("Error al buscar la planta: ", error);
+        res.status(500).json({ error: "Error al buscar la planta" });
+      });
+  } catch (error) {
+    console.error("Unexpected error: ", error);
+    res.status(500).json({ error: "Unexpected error occurred" });
+  }
+}
+
+export function identifyPlant(req, res) {
   const { images } = req.body;
 
   const data = {
@@ -15,58 +47,101 @@ export async function identifyPlant(req, res) {
   };
 
   try {
-    const response = await fetch(`${API_URL}/identification`, {
+    fetch(`${API_URL}/identification`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "Api-Key": API_KEY,
       },
       body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    return res.json(response);
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((jsonData) => res.json(jsonData))
+      .catch((error) => {
+        console.error("Error al identificar la planta: ", error);
+        res.status(500).json({ error: "Error al identificar la planta" });
+      });
   } catch (error) {
-    console.error("Error al identificar la planta: ", error);
-    res.status(500).json({ error: "Error al identificar la planta" });
+    console.error("Unexpected error: ", error);
+    res.status(500).json({ error: "Unexpected error occurred" });
   }
 }
 
-export async function getChatbotConversation(req, res) {
-  const { access_token, question, prompt, temperature, app_name } = req.body;
+export function getPlantDetail(req, res) {
+  const { access_token } = req.params;
+  const { language } = req.query;
+  const details =
+    "common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods";
+
+  try {
+    fetch(
+      `${API_URL}/kb/plants/${access_token}?details=${details}&language=${
+        language || "en"
+      }`,
+      {
+        method: "GET",
+        headers: {
+          "Api-Key": API_KEY,
+        },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((jsonData) => res.json(jsonData))
+      .catch((error) => {
+        console.error("Error al obtener los detalles de la planta: ", error);
+        res
+          .status(500)
+          .json({ error: "Error al obtener los detalles de la planta" });
+      });
+  } catch (error) {
+    console.error("Unexpected error: ", error);
+    res.status(500).json({ error: "Unexpected error occurred" });
+  }
+}
+
+export function getChatbotConversation(req, res) {
+  const { access_token, question, prompt } = req.body;
 
   const data = {
     question,
     prompt,
-    temperature,
-    app_name,
+    app_name: "asd",
   };
 
   try {
-    const response = await fetch(
-      `${API_URL}/identification/${access_token}/conversation`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Api-Key": API_KEY,
-        },
-        body: JSON.stringify(data),
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    res.json(response);
+    fetch(`${API_URL}/identification/${access_token}/conversation`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Api-Key": API_KEY,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((jsonData) => res.json(jsonData))
+      .catch((error) => {
+        console.error("Error al obtener la conversación del chatbot: ", error);
+        res
+          .status(500)
+          .json({ error: "Error al obtener la conversación del chatbot" });
+      });
   } catch (error) {
-    console.error("Error al obtener la conversación del chatbot: ", error);
-    res
-      .status(500)
-      .json({ error: "Error al obtener la conversación del chatbot" });
+    console.error("Unexpected error: ", error);
+    res.status(500).json({ error: "Unexpected error occurred" });
   }
 }
